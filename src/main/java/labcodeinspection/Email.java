01dde8a58b2c2f3056bc7e8f1518623a5b11872f
jreyesr <jreyesr@users.noreply.github.com>
2020-06-23 20:50:37 -0500

    Implement PMD rules and fix violations

--- src/main/java/labcodeinspection/Email.java
+++ src/main/java/labcodeinspection/Email.java
@@ -1,25 +1,35 @@
 package labcodeinspection;
 
+import java.util.Locale;
+
 public class Email {
 
-	private String m_firstName;
-	private String m_lastName;
-	private String password = null;
+	private final String firstName;
+	private final String lastName;
+	private transient String password; // Setter does not make sense here
 	private String department;
-	private int defaultpasswordLength = 8;
-	private String email;
+	private final static int DEFAULT_PWD_LEN = 8;
+	private transient String email; // Setter does not make sense
 
-	public Email(String firstName, String lastName) {
-		this.m_firstName = firstName;
-		this.m_lastName = lastName;
+	/**
+	 * Create on object of class Email
+	 * @param firstName The first name of the employee
+	 * @param lastName The last name of the employee
+	 */
+	public Email(final String firstName, final String lastName) {
+		this.firstName = firstName;
+		this.lastName = lastName;
 	}
 
+	/**
+	 * Prints some information about the object to stdout
+	 */
 	public void showInfo() {
-		System.out.println("\nFIRST NAME= " + m_firstName + "\nLAST NAME= " + m_lastName);
-		System.out.println("DEPARMENT= " + department + "\nEMAIL= " + email + "\nPASSWORD= " + password);
+		System.out.println("\nFIRST NAME= " + getFirstName() + "\nLAST NAME= " + getLastName());
+		System.out.println("DEPARMENT= " + getDepartment() + "\nEMAIL= " + getEmail() + "\nPASSWORD= " + getPassword());
 	}
 
-	public void setDeparment(int depChoice) {
+	public void setDepartment(final int depChoice) {
 		switch (depChoice) {
 		case 1:
 			this.department = "sales";
@@ -30,22 +40,49 @@ public class Email {
 		case 3:
 			this.department = "acct";
 			break;
+		default:
+			break;
 		}
 	}
 
-	private String randomPassword(int length) {
-		String set = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890#$&@*";
+	private String randomPassword(final int length) {
+		final String set = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890#$&@*";
 		char[] password = new char[length];
 		for (int i = 0; i < length; i++) {
-			int rand = (int) (Math.random() * set.length());
+			final int rand = (int) (Math.random() * set.length());
 			password[i] = set.charAt(rand);
 		}
 		return new String(password);
 	}
 
+	/**
+	 * Create an email for the user based on its names. Also set password to a random value
+	 */
 	public void generateEmail() {
-		this.password = this.randomPassword(this.defaultpasswordLength);
-		this.email = this.m_firstName.toLowerCase() + this.m_lastName.toLowerCase() + "@" + this.department
+		this.password = this.randomPassword(DEFAULT_PWD_LEN);
+		this.email = this.getFirstName().toLowerCase(Locale.forLanguageTag("es")) 
+				+ this.getLastName().toLowerCase(Locale.forLanguageTag("es")) 
+				+ "@" + this.getDepartment()
 				+ ".espol.edu.ec";
+	}
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public String getDepartment() {
+		return department;
+	}
+
+	public String getEmail() {
+		return email;
 	}
 }
